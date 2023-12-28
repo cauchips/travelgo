c195f3d43aa25a3caec1d5c57fed2d2ab6ecb997
Jonathan Chandra Hermawan <jonathanchandra17@gmail.com>
2023-12-28 14:14:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // Other configurations can be added here
+  build: {
+    // Specify the output directory (default is 'dist')
+    outDir: 'build',
+
+    // Enable minification for production builds
+    minify: 'terser',
+
+    // Configure external dependencies for smaller bundle size
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+
+  // Development server configurations
+  server: {
+    // Customize the port the dev server runs on
+    port: 3000,
+
+    // Proxy API requests to a different backend during development
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
+  },
+
+  // Define environment variables
+  define: {
+    'process.env.NODE_ENV': process.env.NODE_ENV,
+  },
+
+  // Enable or disable CSS extraction in production
+  css: {
+    extract: true,
+  },
+
+  // Configure additional paths and aliases
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@styles': '/src/styles',
+    },
+  },
+});
